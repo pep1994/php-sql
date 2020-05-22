3bfaa5c33892a779eaa66e0af4127504aca9bcce
Marco Petrini <marcopetrini1994@gmail.com>
2020-05-22 13:02:10 +0200

    code refactoring

--- getAllPaganti.php
+++ getAllPaganti.php
@@ -1,37 +1,11 @@
 <?php
-    // header('Content-Type: application/json');
-    $server = 'localhost';
-    $username = 'root';
-    $password = 'root';
-    $dbName = 'Hotel2';
-    $conn = new mysqli($server, $username, $password, $dbName);
 
-    if ($conn -> connect_errno) {
-        echo $conn -> connect_errno;
-        return;
-    }
+    require  "./partials/functions.php";
 
-    $results = $conn -> query("
-        SELECT name, lastname, address
-        FROM `paganti`
-    ");
+   toHotel2();
 
-    if ($results -> num_rows < 1) {
-        echo "no result";
-        return;
-    }
-
-    $res = [];
-
-    while ($row = $results -> fetch_assoc()) {
-
-         $res[] =  $row;
-    }
-
-    $conn -> close();
-
-    echo json_encode($res);
-
+    
+    
 
 
 
---
+++ partials/functions.php
@@ -0,0 +1,42 @@
+<?php  
+
+
+    function toHotel2 () {
+
+        header('Content-Type: application/json');
+        $server = 'localhost';
+        $username = 'root';
+        $password = 'root';
+        $dbName = 'Hotel2';
+        $conn = new mysqli($server, $username, $password, $dbName);
+
+        if ($conn -> connect_errno) {
+            echo $conn -> connect_errno;
+            return;
+        }
+
+        $results = $conn -> query("
+            SELECT name, lastname, address
+            FROM `paganti`
+        ");
+
+        if ($results -> num_rows < 1) {
+            echo "no result";
+            return;
+        }
+
+        $res = [];
+
+        while ($row = $results -> fetch_assoc()) {
+
+            $res[] =  $row;
+
+        }
+
+        $conn -> close();
+
+        echo json_encode($res);
+
+    }
+
+    var_dump(__DIR__);
